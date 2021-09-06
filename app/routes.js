fa3a7ffe8bdbb0dd40c235e80a87d7091119ff48
Frankie Roberto <frankie@frankieroberto.com>
2021-09-06 16:38:55 +0100

    Add first table

--- app/routes.js
+++ app/routes.js
@@ -4,3 +4,7 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 module.exports = router
+
+router.get('/:yyyy-:mm', async (req, res) => {
+  res.render('month')
+})
